80cc6203ce39553303748a285e80fb877a9cedac
morphias2004 <68760693+morphias2004@users.noreply.github.com>
2022-05-15 09:00:38 +1000

    Add files via upload

---
+++ wvautobrightness.py
@@ -0,0 +1,40 @@
+# PiAutoDim
+# Auto-Dimming for Waveshare DSI Touchscreen LCD Displays
+# Based on https://gpiozero.readthedocs.io/en/stable/recipes.html#light-sensor
+
+from gpiozero import LightSensor
+import time
+
+# Desired maximum & minimum backlight values
+max = 0
+min = 230
+prev = 0
+offset = 10
+delta = 0
+
+# Open backlight control 'file' for writing
+f = open('/sys/waveshare/rpi_backlight/brightness', 'w')
+
+# Instantiate sensor object, set charge time suitable for 10uF capacitor
+sensor = LightSensor(18, charge_time_limit=0.2, threshold=0.1)
+
+while True:
+    # Get light sensor value, map to 0-255
+    lightValue = round(255-(255 * sensor.value))
+
+    # Keep values within defined maximum-minimum values
+    if lightValue < 40:
+        lightValue = max
+    if lightValue > min:
+        lightValue = min
+    
+    # Calculate change in brightness value
+    delta = abs(lightValue - prev)
+
+    # Only write (change) backlight value if there's sensor level change
+    if delta > 5:
+        prev = lightValue
+        f.seek(0)
+        f.write(str(lightValue))
+        f.truncate()
+    time.sleep(1)
